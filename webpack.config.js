ad197b5f4b956cbf7dbbdc378b4745db4d0309ea
Matthew James Collins <you@example.com>
2024-01-19 20:21:49 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Adjust the entry point based on your project structure
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+};
